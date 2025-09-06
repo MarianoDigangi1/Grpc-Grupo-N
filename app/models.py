803fd510ab3931d6ac3147b49dc1997009234d11
nachogomezrp <nachogomezz0011@gmai.com>
2025-09-06 15:56:07 -0300

    Agrego modelo usuario

---
+++ app/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer, String, Boolean, Enum
+from .database import Base
+import enum
+
+class RolEnum(str, enum.Enum):
+    Presidente = "Presidente"
+    Vocal = "Vocal"
+    Coordinador = "Coordinador"
+    Voluntario = "Voluntario"
+
+class Usuario(Base):
+    tablename = "usuarios"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nombreUsuario = Column(String(50), unique=True, nullable=False)
+    nombre = Column(String(100), nullable=False)
+    apellido = Column(String(100), nullable=False)
+    telefono = Column(String(20))
+    clave = Column(String(255), nullable=False) 
+    emailUnico = Column(String(100), unique=True, nullable=False)
+    rol = Column(Enum(RolEnum), nullable=False)
+    estaActivo = Column(Boolean, default=True, nullable=False)
